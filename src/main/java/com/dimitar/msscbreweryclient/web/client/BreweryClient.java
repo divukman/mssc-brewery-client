9891e0f90d15fd4eff94936dfc0a31874d13c7ab
divukman <divukman@gmail.com>
2019-10-01 17:11:00 +0200

    Rest template + unit test.

---
+++ src/main/java/com/dimitar/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,32 @@
+package com.dimitar.msscbreweryclient.web.client;
+
+import com.dimitar.msscbreweryclient.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    private String apiHost;
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+
+    public void setApiHost(String apiHost) {
+        this.apiHost = apiHost;
+    }
+}
